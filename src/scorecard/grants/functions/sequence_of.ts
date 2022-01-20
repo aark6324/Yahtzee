a5c68b7c9479f10a25d25fc8f2cc5b6a0e53ded5
aark <aark6324@gmail.com>
2022-01-20 15:50:24 +0100

    Use same naming scheme

--- src/scorecard/grants/functions/sequence_of.ts
+++ src/scorecard/grants/functions/sequence_of.ts
@@ -1,12 +1,12 @@
 import type { AllDiceResults, Points } from "../../../types";
 
-export const sequence_of = (allDiceResults: AllDiceResults, sequenceLength: number, assignedPoints: Points): Points => {
-    allDiceResults.sort();
+export const sequence_of = (allResults: AllDiceResults, sequenceLength: number, assignedPoints: Points): Points => {
+    allResults.sort();
     let currentSeq = 0;
     let highestSeq = 0;
-    for (let i=0; i<allDiceResults.length - 1; i++) {
-        if (allDiceResults[i] === allDiceResults[i+1]) continue;
-        if (allDiceResults[i] === allDiceResults[i+1] - 1) {
+    for (let i=0; i<allResults.length - 1; i++) {
+        if (allResults[i] === allResults[i+1]) continue;
+        if (allResults[i] === allResults[i+1] - 1) {
             currentSeq++;
         } else {
             currentSeq = 0;
