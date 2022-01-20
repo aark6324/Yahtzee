f5fd8d12cd820cb786404d18a431b58b9f072d2e
aark <aark6324@gmail.com>
2022-01-20 12:49:32 +0100

    New Scorecard

---
+++ src/scorecard/scorecard.test.ts
@@ -0,0 +1,33 @@
+import { Scorecard } from "./scorecard";
+
+describe("Scorecard", () => {
+    let scorecard: Scorecard;
+    beforeEach(() => {
+        scorecard = new Scorecard();
+    })
+    it("returns 9 granted points", () => {
+        const points = scorecard.getGrantedPoints("threes", [3,5,4,3,2,3]);
+        expect(points).toEqual(9);
+    })
+    it("returns null on unclaimed category", () => {
+        const points = scorecard.getClaimedPoints("threes");
+        expect(points).toEqual(null);
+    })
+    it("returns 7 points on claimed category", () => {
+        scorecard.claimGrantedPoints("three_of_a_kind", [1,1,1,2,2]);
+        const points = scorecard.getClaimedPoints("three_of_a_kind");
+        expect(points).toEqual(7)
+    })
+    it("returns 0 points on claimed category", () => {
+        scorecard.claimGrantedPoints("three_of_a_kind", [2,5,6,3,2,1]);
+        const points = scorecard.getClaimedPoints("three_of_a_kind");
+        expect(points).toEqual(0);
+    })
+    it("throws an error trying to claim a claimed category", () => {
+        scorecard.claimGrantedPoints("three_of_a_kind", [4,4,4,3,2]);
+        expect(() => {
+            scorecard.claimGrantedPoints("three_of_a_kind", [3,5,2,3,3])
+        }).toThrowError();
+    })
+
+})
--- src/scorecard/scorecard.ts
+++ src/scorecard/scorecard.ts
@@ -1,13 +1,42 @@
 import { allCategories } from "./categories"
-import type { CategoryCollection } from "./categories"
+import type { CategoryCollection, CategoryKey } from "./categories"
+import type { AllDiceResults, Points } from "../types";
+
+
+type Scores = Partial<Record<CategoryKey, {
+    Points: null | Points
+}>>
+
 
 class Scorecard {
     categories: CategoryCollection;
+    scores: Scores;
 
     constructor() {
-        this.categories = allCategories;
+        this.scores = {};
+    }
+
+    getGrantedPoints(categoryKey: CategoryKey, allDiceResults: AllDiceResults): Points {
+        return allCategories[categoryKey].grantedPoints(allDiceResults);
+    }
+
+    claimGrantedPoints(categoryKey: CategoryKey, allDiceResults: AllDiceResults): Points {
+        if (this.scores[categoryKey]) {
+            throw new Error("Cannot claim an already claimed category");
+        } else {
+            const points = allCategories[categoryKey].grantedPoints(allDiceResults);
+            this.scores[categoryKey] = { Points: points };
+            return points;
+        }
+    }
+
+    getClaimedPoints(categoryKey: CategoryKey): Points {
+        return this.scores.hasOwnProperty(categoryKey) ? this.scores[categoryKey].Points : null;
+    }
+
+    isCategoryClaimed(categoryKey: CategoryKey): boolean {
+        return this.scores.hasOwnProperty(categoryKey);
     }
 }
 
-let sc = new Scorecard();
-
+export { Scorecard }
