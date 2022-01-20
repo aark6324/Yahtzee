36f03f40d8da79f85d11ae0cae17a82e0f00dacb
aark <aark6324@gmail.com>
2022-01-20 12:50:39 +0100

    Better implementation using reduce

--- src/scorecard/grants/functions/sum_of_occurrences.ts
+++ src/scorecard/grants/functions/sum_of_occurrences.ts
@@ -1,11 +1,8 @@
 import type { AllDiceResults, Points } from "../../../types";
 
-export const sum_of_occurences = (allResults: AllDiceResults, valueToCount: number): Points => {
-    let sum = 0;
-    for (let i in allResults) {
-        if (allResults[i] == valueToCount) {
-            sum += valueToCount;
-        }
-    }
-    return sum;
-}
+
+export const sum_of_occurences = (allResults: AllDiceResults, valueToCount: number): Points =>
+    allResults.reduce(
+        (accumulated, current) => current === valueToCount ? accumulated + current : accumulated
+        , 0
+    );
