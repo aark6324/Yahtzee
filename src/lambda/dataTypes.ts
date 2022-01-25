56758a36b19c021c5f72f660aa7979bb46023a08
aark <aark6324@gmail.com>
2022-01-25 16:45:45 +0100

    Add dataTypes for storing game information

---
+++ src/lambda/dataTypes.ts
@@ -0,0 +1,37 @@
+import { AWSError } from "aws-sdk";
+import { Scores } from "../scorecard/scorecard";
+
+
+export type Identifier = string;
+
+export interface Player {
+    playerID: string
+}
+
+
+
+export interface PlayerScorecard {
+    playerID: string,
+    scores: Scores
+}
+
+export type PlayerCount = number;
+
+export type GameStatus = "not_started" | "playing" | "finished";
+
+
+export interface PlayerSession {
+    playerID: Identifier
+    scores: Scores
+    entitiyID: string
+    nodeID: string
+}
+
+export interface Game {
+    status: GameStatus
+    max_players: PlayerCount
+    current_players: PlayerCount
+    winner: Player | undefined
+    entitiyID: string
+    nodeID: "game"
+}
