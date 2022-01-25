25006a388b7caa715bf49bc69145ad4860a258b2
aark <aark6324@gmail.com>
2022-01-25 16:42:09 +0100

    Add utils for creating data objects

---
+++ src/lambda/utils.ts
@@ -0,0 +1,41 @@
+import { Player, Identifier, GameStatus, PlayerCount, Game } from "./dataTypes";
+import { nanoid } from "nanoid";
+import { PlayerSession } from "./dataTypes";
+
+export const createPlayer = (ID: Identifier = undefined): Player => {
+    if (ID === undefined) ID = nanoid();
+    let player: Player = {
+        playerID: ID
+    }
+
+    return player;
+}
+
+export const createGameItem = (max_players: PlayerCount = 2, gameID: Identifier = undefined): Game => {
+
+    if (gameID === undefined) gameID = nanoid();
+
+    let gameItem: Game = {
+        status: "not_started",
+        max_players,
+        winner: undefined,
+        current_players: 0,
+        entitiyID: `game#${gameID}`,
+        nodeID: "game"
+    }
+
+    return gameItem;
+}
+
+
+export const createPlayerSession = (gameID: Identifier, playerID: Identifier): PlayerSession => {
+
+    let gameSession: PlayerSession = {
+        playerID,
+        scores: {},
+        entitiyID: `game#${gameID}`,
+        nodeID: `player#${playerID}`,
+    }
+    
+    return gameSession;
+}
