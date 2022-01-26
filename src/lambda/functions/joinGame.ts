c9332a5ab46c774495c65e92e82231559e138bb5
aark <aark6324@gmail.com>
2022-01-26 10:06:24 +0100

    Add joinGame endpoint

--- src/lambda/functions/joinGame.ts
+++ src/lambda/functions/joinGame.ts
@@ -1,43 +1,68 @@
-import {APIGatewayProxyHandler} from 'aws-lambda';
-import { nanoid } from "nanoid";
-import {DocumentClient} from 'aws-sdk/clients/dynamodb';
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { nanoid } from 'nanoid';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 import { Scorecard } from '../../scorecard/scorecard';
 import { Game, GameStatus, Identifier, Player, PlayerCount, PlayerSession } from '../dataTypes';
 import { createPlayer, createPlayerSession } from '../utils';
 
-const client = new DocumentClient({region: 'eu-north-1'});
-
+const client = new DocumentClient({ region: 'eu-north-1' });
 
 interface JoinGameRequest {
-    gameID: Identifier
-    playerID: string
+    gameID: Identifier;
+    playerID: string;
 }
 
 interface JoinGameResponse {
-    success: boolean
-    gameStatus: GameStatus
-    maxPlayers: PlayerCount
-    currentPlayers: PlayerCount
+    success: boolean;
+    gameStatus?: GameStatus;
+    maxPlayers?: PlayerCount;
+    currentPlayers?: PlayerCount;
 }
 
+async function getGame(gameID: Identifier): Promise<Game | undefined> {
+    let dynoRes = await client
+        .get({
+            TableName: process.env.TABLE_NAME,
+            Key: {
+                entitiyID: gameID,
+                nodeID: 'game',
+            },
+        })
+        .promise();
+
+    return dynoRes.Item as Game;
+}
+
+async function putPlayerSession(playerSession: PlayerSession) {
+    await client
+        .put({
+            TableName: process.env.TABLE_NAME,
+            Item: playerSession,
+        })
+        .promise();
+}
 
 export const handler: APIGatewayProxyHandler = async (event, context): Promise<any> => {
+    let response: JoinGameResponse;
 
     let body: JoinGameRequest = JSON.parse(event.body);
 
     let playerSession: PlayerSession = createPlayerSession(body.gameID, body.playerID);
 
-    
-    // TODO: Add condition expression and create JoinGameResponse
-    // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.OperatorsAndFunctions.html
-    let dynoRes = await client.put({
-        TableName: process.env.TABLE_NAME,
-        Item: playerSession,
-        ConditionExpression: ""
-    }).promise()
+    const game = await getGame(body.gameID);
 
-    let response: JoinGameResponse;
+    if (game && game.current_players < game.max_players) {
+        await putPlayerSession(playerSession);
+        response = {
+            success: true,
+            gameStatus: game.status,
+            currentPlayers: game.current_players,
+            maxPlayers: game.max_players,
+        };
+    } else {
+        response = { success: false };
+    }
+
     return response;
-
-}
+};
